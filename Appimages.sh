e3bbe7d89c4db3e573ab4cb91b1a38ea999e08a5
sfab07 <sfab07@gmail.com>
2020-01-13 23:25:18 +0100

    Update Appimages.sh

--- Appimages.sh
+++ Appimages.sh
@@ -18,7 +18,8 @@ fi
 case "${choice}" in
     *"Joplin"* )
     echo "# Téléchargements de Joplin"
-    wget https://github.com/laurent22/joplin/releases/download/v1.0.175/Joplin-1.0.175-x86_64.AppImage
+    wget -P ~/Applications https://github.com/laurent22/joplin/releases/download/v1.0.175/Joplin-1.0.175-x86_64.AppImage
+    chmod -R a+x ~/Applications
     echo "10"
     ;;
 esac
@@ -26,7 +27,8 @@ esac
 case "${choice}" in
     *"MolotovTV"* )
     echo "# Téléchargements de Molotov TV"
-    wget http://desktop-auto-upgrade.molotov.tv/linux/4.2.2/molotov.AppImage
+    wget -P ~/Applications http://desktop-auto-upgrade.molotov.tv/linux/4.2.2/molotov.AppImage
+    chmod -R a+x ~/Applications
     echo "10"
     ;;
 esac
@@ -34,7 +36,8 @@ esac
 case "${choice}" in
     *"Odio"* )
     echo "# Installation de Odio"
-    wget https://github.com/odioapp/odio/releases/download/v1.4.0/odio-1.4.0-x86_64.AppImage
+    wget -P ~/Applications https://github.com/odioapp/odio/releases/download/v1.4.0/odio-1.4.0-x86_64.AppImage
+    chmod -R a+x ~/Applications
     echo "10"
     ;;
 esac
@@ -42,7 +45,8 @@ esac
 case "${choice}" in
     *"Lahda"* )
     echo "# Installation de Lahda"
-    wget https://github.com/jestrux/ladha/releases/download/0.5.2/ladha-0.5.2-x86_64.AppImage
+    wget -P ~/Applications https://github.com/jestrux/ladha/releases/download/0.5.2/ladha-0.5.2-x86_64.AppImage
+    chmod -R a+x ~/Applications
     echo "10"
     ;;
 esac
@@ -50,7 +54,8 @@ esac
 case "${choice}" in
     *"MellowPlayer"* )
     echo "# Installation de MellowPlayer"
-    wget https://github.com/ColinDuquesnoy/MellowPlayer/releases/download/3.4.0/MellowPlayer-x86_64.AppImage
+    wget -P ~/Applications https://github.com/ColinDuquesnoy/MellowPlayer/releases/download/3.4.0/MellowPlayer-x86_64.AppImage
+    chmod -R a+x ~/Applications
     echo "10"
     ;;
 esac
@@ -58,7 +63,8 @@ esac
 case "${choice}" in
     *"Playme"* )
     echo "# Installation de Playme"
-    wget https://github.com/lamazingco/playme/releases/download/v1.0.3/playme-1.0.3-x86_64.AppImage
+    wget -P ~/Applications https://github.com/lamazingco/playme/releases/download/v1.0.3/playme-1.0.3-x86_64.AppImage
+    chmod -R a+x ~/Applications
     echo "10"
     ;;
 esac
@@ -66,7 +72,8 @@ esac
 case "${choice}" in
     *"Clipgrab"* )
     echo "# Installation de Clipgrab"
-    wget https://www.videohelp.com/download/ClipGrab-3.8.9-x86_64.AppImage?r=WgRqBQbzDTSt
+    wget -P ~/Applications https://www.videohelp.com/download/ClipGrab-3.8.9-x86_64.AppImage?r=WgRqBQbzDTSt
+    chmod -R a+x ~/Applications
     echo "10"
     ;;
 esac
